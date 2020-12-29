44f8237090702635d4ea7efdc23457e07985fc0a
Albi Sema <albi.sema@siemens.com>
2020-12-29 12:29:46 +0100

    task3

--- src/chatbot.h
+++ src/chatbot.h
@@ -31,7 +31,7 @@ public:
     ////
     
     ChatBot(const ChatBot& source); // copy constructor
-    ChatBot& operator=(const ChatBot& source); //copy assignment operator
+    ChatBot& operator=(const ChatBot& source); // copy assignment operator
     ChatBot(ChatBot&& source); // move constructor
     ChatBot& operator=(ChatBot&& source); // move assignment operator
 
